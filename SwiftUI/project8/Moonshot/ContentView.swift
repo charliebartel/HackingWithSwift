c138fbcd7b26887f5cb3c222721ec5b4c6b7ad12
charliebartel <charles.bartel@specialized.com>
2022-03-05 21:10:40 -0700

    challenge 3 project 8

--- SwiftUI/project8/Moonshot/ContentView.swift
+++ SwiftUI/project8/Moonshot/ContentView.swift
@@ -7,6 +7,41 @@
 
 import SwiftUI
 
+struct ApolloListView: View {
+    let mission: Mission
+
+    var body: some View {
+        HStack {
+            Image(mission.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 60, height: 60)
+                .padding(5)
+
+            VStack {
+                Text(mission.displayName)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 5)
+
+                Text(mission.formattedLaunchDate)
+                    .font(.caption)
+                    .foregroundColor(.white.opacity(0.5))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 5)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.lightBackground)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(.lightBackground)
+        )
+    }
+}
+
 struct ApolloCardView: View {
     let mission: Mission
 
@@ -39,31 +74,71 @@ struct ApolloCardView: View {
     }
 }
 
-struct ContentView: View {
-    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    let missions: [Mission] = Bundle.main.decode("missions.json")
+struct GridView: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
 
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
 
     var body: some View {
+        LazyVGrid(columns: columns) {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    ApolloCardView(mission: mission)
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+    }
+}
+
+struct ListView: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+
+    var body: some View {
+        LazyVStack {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    ApolloListView(mission: mission)
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+    }
+}
+
+struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    @State private var showingGrid = true
+
+    var body: some View {
         NavigationView {
             ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            ApolloCardView(mission: mission)
-                        }
-                    }
+                if showingGrid {
+                    GridView(astronauts: astronauts, missions: missions)
+                } else {
+                    ListView(astronauts: astronauts, missions: missions)
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Toggle View") {
+                        showingGrid.toggle()
+                    }
+                }
+            }
         }
     }
 }
